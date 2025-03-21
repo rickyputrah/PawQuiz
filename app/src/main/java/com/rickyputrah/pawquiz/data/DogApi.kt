6853dbf7f62101ee3e258d4abbf6b774a1317e8f
rickyputrah <rickyph96@gmail.com>
2025-03-21 22:02:44 +0800

    Fetch Dog Image from API with provided breed code

--- app/src/main/java/com/rickyputrah/pawquiz/data/DogApi.kt
+++ app/src/main/java/com/rickyputrah/pawquiz/data/DogApi.kt
@@ -3,13 +3,24 @@ package com.rickyputrah.pawquiz.data
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface DogApi {
     @GET("breeds/list/all")
     suspend fun getBreedList(): DogBreedList
+
+    @GET("breed/{breed}/images/random")
+    suspend fun getDogImage(
+        @Path("breed", encoded = true) breed: String
+    ): DogImage
 }
 
 @JsonClass(generateAdapter = true)
 data class DogBreedList(
     @Json(name = "message") val message: Map<String, List<String>>,
+)
+
+@JsonClass(generateAdapter = true)
+data class DogImage(
+    @field:Json(name = "message") val imageUrl: String
 )
